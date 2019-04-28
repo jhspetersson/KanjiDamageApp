702dcddf2d0e5f180e62dec0017f5bb4d7a82d49
xenozoid <xenozoid@gmail.com>
2019-04-28 09:27:24 +0300

    make following links in About window possible

--- app/src/main/java/com/kanjidamage/app/AboutActivity.java
+++ app/src/main/java/com/kanjidamage/app/AboutActivity.java
@@ -1,7 +1,11 @@
 package com.kanjidamage.app;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        final TextView kdLink = findViewById(R.id.kd_url);
+        kdLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = kdLink.getText().toString();
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        final TextView authorLink = findViewById(R.id.author_url);
+        authorLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = authorLink.getText().toString();
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
     }
 }
